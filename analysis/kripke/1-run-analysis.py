98ad978d98bea381fc160c6a1c3157eaca0e147e
vsoch <vsoch@users.noreply.github.com>
2025-04-18 13:53:07 -0600

    analysis results: kripke on gke
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- analysis/kripke/1-run-analysis.py
+++ analysis/kripke/1-run-analysis.py
@@ -64,7 +64,7 @@ def main():
         os.makedirs(outdir)
 
     # Find input files (skip anything with test)
-    files = ps.find_inputs(indir, "lammps")
+    files = ps.find_inputs(indir, "kripke")
     if not files:
         raise ValueError(f"There are no input files in {indir}")
 
@@ -72,18 +72,25 @@ def main():
     df = parse_data(indir, outdir, files)
     plot_results(df, outdir, args.non_anon)
 
-def parse_matom_steps(item):
-    """
-    Parse matom steps
 
-    We separated this into a function because cyclecloud submits have
-    two problem sizes in one file.
+
+def parse_kripke_foms(item):
     """
-    # Add in Matom steps - what is considered the LAMMPS FOM
-    # https://asc.llnl.gov/sites/asc/files/2020-09/CORAL2_Benchmark_Summary_LAMMPS.pdf
-    # Not parsed by metrics operator so we find the line here
-    line = [x for x in item.split("\n") if "Matom-step/s" in x][0]
-    return float(line.split(",")[-1].strip().split(" ")[0])
+    Figures of Merit
+    ================
+
+      Throughput:         2.683674e+10 [unknowns/(second/iteration)]
+      Grind time :        3.726235e-11 [(seconds/iteration)/unknowns]
+      Sweep efficiency :  17.43900 [100.0 * SweepSubdomain time / SweepSolver time]
+      Number of unknowns: 4932501504
+    """
+    metrics = {}
+    for line in item.split("\n"):
+        if "Grind time" in line:
+            parts = [x for x in line.replace(":", "").split(" ") if x]
+            metrics["grind_time_seconds"] = float(parts[2])
+    return metrics
+
 
 
 def parse_data(indir, outdir, files):
@@ -91,7 +98,7 @@ def parse_data(indir, outdir, files):
     Parse filepaths for environment, etc., and results files for data.
     """
     # metrics here will be figures of merit, and seconds runtime
-    p = ps.ProblemSizeParser("lammps")
+    p = ps.ResultParser("kripke")
 
     # For flux we can save jobspecs and other event data
     data = {}
@@ -111,25 +118,17 @@ def parse_data(indir, outdir, files):
          
         item = ps.read_file(filename)
         jobs = ps.parse_flux_jobs(item)
-        # All had the same problem size
-        problem_size = "32x16x16"
-        for job, metadata in jobs.items():
-            p.add_result(
-                "matom_steps_per_second", parse_matom_steps(metadata['log']), problem_size
-            )   
-            wall_time = [
-                ps.convert_walltime_to_seconds(x.rsplit(" ", 1)[-1])
-                for x in metadata['log'].split("\n")
-                if "Total wall time" in x
-            ][0]        
-            p.add_result("wall-time", wall_time, problem_size)
-            p.add_result("duration", metadata['duration'], problem_size)
-            p.add_result("hookup-time", metadata['duration'] - wall_time, problem_size)
 
-    print("Done parsing lammps results!")
+        for job, metadata in jobs.items():
+            metrics = parse_kripke_foms(metadata['log'])
+            for metric, value in metrics.items():
+                p.add_result(metric, value)
+            p.add_result("duration", metadata['duration'])
+
+    print("Done parsing kripke results!")
 
     # Save stuff to file first
-    p.df.to_csv(os.path.join(outdir, "lammps-results.csv"))
+    p.df.to_csv(os.path.join(outdir, "kripke-results.csv"))
     ps.write_json(jobs, os.path.join(outdir, "flux-jobs-and-events.json"))
     return p.df
 
@@ -186,12 +185,12 @@ def plot_results(df, outdir, non_anon=False):
             palette=cloud_colors,
             order=[4, 8, 16, 32, 64],
         )
-        if metric in ["duration", "wall-time", "hookup-time"]:        
-            axes[0].set_title(f"LAMMPS {metric.capitalize()} (CPU)", fontsize=14)
+        if "grind" not in metric:        
+            axes[0].set_title(f"Kripke {metric.capitalize()} (CPU)", fontsize=14)
             axes[0].set_ylabel("Seconds", fontsize=14)
         else:
-            axes[0].set_title("LAMMPS M/Atom Steps per Second (CPU)", fontsize=14)
-            axes[0].set_ylabel("M/Atom Steps Per Second", fontsize=14)
+            axes[0].set_title("Kripke Grind Time (CPU)", fontsize=14)
+            axes[0].set_ylabel("Grind Time (Seconds)", fontsize=14)
         axes[0].set_xlabel("Nodes", fontsize=14)
 
         handles, labels = axes[0].get_legend_handles_labels()
@@ -204,8 +203,8 @@ def plot_results(df, outdir, non_anon=False):
         axes[1].axis("off")
     
         plt.tight_layout()
-        plt.savefig(os.path.join(img_outdir, f"lammps-{metric}-cpu.svg"))
-        plt.savefig(os.path.join(img_outdir, f"lammps-{metric}-cpu.png"))
+        plt.savefig(os.path.join(img_outdir, f"kripke-{metric}-cpu.svg"))
+        plt.savefig(os.path.join(img_outdir, f"kripke-{metric}-cpu.png"))
         plt.clf()
 
         # Print the total number of data points
